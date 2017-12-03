81eed2b512fd2c5ae46decbe9f52f74095e01bc1
Enamul Haque <shawoncse12@gmail.com>
2017-12-03 22:03:00 +0600

    Add files via upload

---
+++ deep_version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 11:33:36 2017
+
+@author: haque
+"""
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
